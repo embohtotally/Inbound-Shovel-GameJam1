142bd9c8200f431064142217869d3bd2428e200d
Naufalafif2015 <naufalafif2015@gmail.com>
2025-07-19 12:07:57 +0700

    Update #2
    Tambahan dari Naufal

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Exit : MonoBehaviour
+{
+    // Call this method when the exit button is clicked
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+}
